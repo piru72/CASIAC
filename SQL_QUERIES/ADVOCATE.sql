af9d39f4b2ead43a09a1c18e75e6c9fb44fc03d0
piru72 <parvezdirom2000@gmail.com>
2023-01-27 03:13:53 +0600

    add (query) : query added

--- SQL_QUERIES/ADVOCATE.sql
+++ SQL_QUERIES/ADVOCATE.sql
@@ -13,7 +13,7 @@ Create Table Advocate(
 
 );
 
-
+--> INSERT DATAS INTO THE TABLE
 Insert into Advocate
 values('Super','ADMIN','casiacAdmin12@yahoo.com','01790603952','Syltherin','2001-06-20','Female')
 Insert into Advocate
@@ -26,9 +26,71 @@ Insert into Advocate
 values('Waziha','Afrin','wazzihaa@yahoo.com','0170135589','poik$','1988-09-20','Female')
 
 
-Truncate  table Advocate
-select LEmail From Advocate
+
+--> SELECT STATEMENTS TO VIEW<--
+
+--> ALL THE DATAS
 Select *From Advocate
 
-SELECT LPass,LFName FROM Advocate WHERE LEmail ='parvezdirom2000@gmai.com' 
-Drop table Advocate
+--> ALL THE MALE LAWYERS
+SELECT * FROM Advocate WHERE LGender = 'MALE'
+
+--> FIRST NAME ADN LAST NAME
+SELECT LFName , LLName FROM Advocate
+
+--> NOT EQUAL
+SELECT LFName , LLName FROM Advocate WHERE LFName <> 'PARVEZ'
+
+--> DISTINCT FIRST NAME 
+SELECT DISTINCT LFName FROM Advocate
+
+--> MERGE TWO COLUMS
+SELECT LFName + ' ' + LLName as 'LAWYER NAME' FROM Advocate
+
+--> ORDER BY ASSCENDING ORDER
+SELECT * FROM Advocate  ORDER BY LFName
+
+--> ORDER BY DESSCENDING ORDER
+SELECT * FROM Advocate  ORDER BY LFName DESC
+
+
+select LEmail From Advocate
+
+
+SELECT LPass,LFName FROM Advocate WHERE LEmail ='parvezdirom2000@gmai.com'
+
+
+--> OPERATORS
+--> LIKE 
+
+
+-- > STARTS WITH TAB AND ENDS WITH ANYTHING
+SELECT * FROM ADVOCATE WHERE LFName LIKE 'TAB%'
+
+--> ENDS WITH UM AND STARTS WITH ANYTHING
+SELECT * FROM ADVOCATE WHERE LFName LIKE '%UM'
+
+--> HAS SU ANYWHERE IN MIDDLE 
+SELECT * FROM ADVOCATE WHERE LFName LIKE '%SU%'
+
+--> THE THIRD CHARACTER IS B
+SELECT * FROM ADVOCATE WHERE LFName LIKE '__B%'
+ 
+
+
+--> DATA DEFINATION LANGUAGE (DDL) THINGS
+
+--> ADD A COULMN
+ALTER TABLE ADVOCATE ADD ADVOCATE_ADDRESS VARCHAR(50) 
+
+--> DELETE A COLUMN
+ALTER TABLE ADVOCATE DROP COLUMN ADVOCATE_ADDRESS
+
+--> CHANE COULMN NAME 
+
+--> DELETE ALL THE DATAS BUT KEEP THE TABLE STATUS SAME AS BEFORE
+TRUNCATE TABLE ADVOCATE
+--> DROP THE WHOLE TABLE WITH THE DATAS
+DROP TABLE ADVOCATE
+
+
