3498a308bbbcfb357b91fb0b4a5bd04a187d2f87
taralamia <79480117+taralamia@users.noreply.github.com>
2023-01-28 03:56:24 +0600

    Fix(table):User table fixed

--- SQLServerProject/src/Database/DatabaseCredentials.java
+++ SQLServerProject/src/Database/DatabaseCredentials.java
@@ -10,14 +10,14 @@ package Database;
  */
 public class DatabaseCredentials {
 
-    //private static final String URL= "jdbc:sqlserver://TABASSUM\\SQLEXPRESS;databaseName=projectDB;integratedSecurity=true;encrypt=false";
+    private static final String URL= "jdbc:sqlserver://TABASSUM\\SQLEXPRESS;databaseName=projectDB;integratedSecurity=true;encrypt=false";
 
     private static final String PORT = "1433"; // The tcp port number that was setted on the SQL SERVER NETWORK CONFIGURATION -> TCP/IP -> IP ADDRESSESS -> IPAIII
     private static final String SERVER = "localhost"; // AS THE PROJECT IS RUNNING IN lOCALHOST
     private static final String DATABASENAME = "ProjectDB"; // The name of the database that you created using the MSSQL STUDIO
     private static final String SQLPASSWORD = "123456"; // The password you had setted for sa
     private static final String SQLUSERNAME = "sa";
-    private static final String URL = "jdbc:sqlserver://" + SERVER + ":" + PORT + ";databaseName=" + DATABASENAME + ";encrypt=true;trustServerCertificate=true;";
+   // private static final String URL = "jdbc:sqlserver://" + SERVER + ":" + PORT + ";databaseName=" + DATABASENAME + ";encrypt=true;trustServerCertificate=true;";
 
     public String getDatabaseUrl() {
       return URL;
